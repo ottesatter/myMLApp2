6319be4e88f218ef4706d98e6427657ddd43b9a7
otte <110157@st-maartenscollege.nl>
2021-01-11 16:32:52 +0100

    Add project files.

---
+++ myMLApp2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace myMLApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
